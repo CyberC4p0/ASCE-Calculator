738eac3499f71013d3fdef2c301c77e98f05175a
CyberC4p0 <jmembreno90217@gmail.com>
2022-12-09 00:49:13 -0500

    Created the skeleton & provided sample output on README.md

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # ASCE-Calculator
 A calculator to automate various structural calculations
 
+SAMPLE OUTPUT:
+
+```
+ASCE Calculator
+
+15.025 <= 45.5  OK
+21.325 <= 45.5  OK
+21.325 <= 45.5  OK
+27.625 <= 45.5  OK
+```
+
 Disclaimer: Not connected/related in any way to the American Society of Civil Engineers
---
+++ main.py
@@ -0,0 +1,32 @@
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+
+green_OK = Fore.GREEN + " OK"
+red_FAIL = Fore.RED + " FAIL"
+
+print("ASCE Calculator\n")
+
+def main():
+  
+    zone_1 = int(71)
+    zone_2r = int(91)
+    zone_2e = int(91)
+    zone_3 = int(111)
+
+    zones = [zone_1,zone_2r,zone_2e,zone_3]
+    amount_of_zones = len(zones)
+
+    aerodynamic_multiplier = float(0.315)
+    Mg = float(7.34)
+    Mf = float(45.5)
+
+    for zone in zones:
+        Mr = round((zone * aerodynamic_multiplier) - Mg, 3)
+        if Mr <= Mf:
+            print(Mr, "<=", Mf, green_OK)
+
+        else:
+            print(Mr, "<=", Mf, red_FAIL)
+
+main()
