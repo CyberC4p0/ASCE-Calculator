20c84a51de80280d3359b145aa96ac35bd4d40b0
cyberc4p0 <jmembreno90217@gmail.com>
2024-08-20 01:15:06 -0400

    Modified what was being outputed

--- README.md
+++ README.md
@@ -4,25 +4,25 @@ A calculator to automate various structural calculations
 **SAMPLE OUTPUT:**
 
 ```
+--------TABLE VALUES------
+
+Aerodynamic Multiplier: 0.315
+Restoring Moments due to Gravity (Mg): 7.34
+Attachment Resistance Expressed as a Moment (Mf): 45.5
+
 -------PRESSURE ZONES-----
 
-Zone 1: -50
-Zone 2e: -60
-Zone 2n: -70
-Zone 2r: -80
-Zone 3: -90
-Zone 3e: -100
-Zone 3r: -110
+Zone 1: -71
+Zone 2e: -91
+Zone 2r: -91
+Zone 3: -111
 
 -----------REPORT---------
 
-Zone 1: 8.41 <= 45.5  OK
-Zone 2r: 17.86 <= 45.5  OK
-Zone 2e: 11.56 <= 45.5  OK
-Zone 2n: 14.71 <= 45.5  OK
-Zone 3: 21.01 <= 45.5  OK
-Zone 3e: 24.16 <= 45.5  OK
-Zone 3r: 27.31 <= 45.5  OK
+Zone 1: (71 x 0.315 = 22.365) - 7.34 = 15.025 <= 45.5  OK
+Zone 2e: (91 x 0.315 = 28.665) - 7.34 = 21.325 <= 45.5  OK
+Zone 2r: (91 x 0.315 = 28.665) - 7.34 = 21.325 <= 45.5  OK
+Zone 3: (111 x 0.315 = 34.965) - 7.34 = 27.625 <= 45.5  OK
 ```
 
 Required Modules:
--- src/main.py
+++ src/main.py
@@ -18,33 +18,52 @@ OK = Fore.GREEN + " OK"
 FAIL = Fore.RED + " FAIL"
 
 frame = customtkinter.CTkFrame(master=gui)
-frame.pack(pady=30, padx=70, fill="both", expand=True)
+frame.pack(pady=15, padx=35, fill="both", expand=True)
 
 title = customtkinter.CTkLabel(master=frame, text="ASCE Section 1 Method 1", )
-title.pack(pady=12, padx=10)
+title.pack(pady=6, padx=5)
+
+aerodynamic_multiplier_input = customtkinter.CTkEntry(master=frame, placeholder_text="Aerodynamic Multiplier")
+aerodynamic_multiplier_input.pack(pady=6, padx=5)
+
+Mg_input = customtkinter.CTkEntry(master=frame, placeholder_text="Mg")
+Mg_input.pack(pady=6, padx=5)
+
+Mf_input = customtkinter.CTkEntry(master=frame, placeholder_text="Mf")
+Mf_input.pack(pady=6, padx=5)
 
 zone_1 = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 1")
-zone_1.pack(pady=12, padx=10)
+zone_1.pack(pady=6, padx=5)
 
 zone_2e = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 2e")
-zone_2e.pack(pady=12, padx=10)
+zone_2e.pack(pady=6, padx=5)
 
 zone_2n = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 2n")
-zone_2n.pack(pady=12, padx=10)
+zone_2n.pack(pady=6, padx=5)
 
 zone_2r = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 2r")
-zone_2r.pack(pady=12, padx=10)
+zone_2r.pack(pady=6, padx=5)
 
 zone_3 = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 3")
-zone_3.pack(pady=12, padx=10)
+zone_3.pack(pady=6, padx=5)
 
 zone_3e = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 3e")
-zone_3e.pack(pady=12, padx=10)
+zone_3e.pack(pady=6, padx=5)
 
 zone_3r = customtkinter.CTkEntry(master=frame, placeholder_text="Zone 3r")
-zone_3r.pack(pady=12, padx=10)
+zone_3r.pack(pady=6, padx=5)
 
 def main():
+
+    print("\n--------TABLE VALUES------\n")
+
+    aerodynamic_multiplier = float(aerodynamic_multiplier_input.get())
+    Mg = float(Mg_input.get())
+    Mf = float(Mf_input.get())
+
+    print("Aerodynamic Multiplier:",aerodynamic_multiplier)
+    print("Restoring Moments due to Gravity (Mg):",Mg)
+    print("Attachment Resistance Expressed as a Moment (Mf):",Mf)
 
     print("\n-------PRESSURE ZONES-----\n")
 
@@ -58,13 +77,15 @@ def main():
             ["Zone 3r",int(zone_3r.get())]
             ]
 
+    for zone in zones:
+        if zone[1] == 0:
+            pass
+        else:
+            print(zone[0] + ": -" + str(zone[1]))
+
     print("\n-----------REPORT---------\n")
     
     amount_of_zones = len(zones)
-
-    aerodynamic_multiplier = float(0.315)
-    Mg = float(7.34)
-    Mf = float(45.5)
 
     for zone in zones:
         if zone[1] == 0:
@@ -72,17 +93,17 @@ def main():
         else:
             Mr = round((zone[1] * aerodynamic_multiplier) - Mg, 3)
             if Mr <= Mf:
-                print(zone[0] + ":", Mr, "<=", Mf, OK)
+                print(zone[0] + ": ("+str(zone[1]),"x",str(aerodynamic_multiplier),"=",str(zone[1] * aerodynamic_multiplier)+") -",str(Mg),"=",str(Mr),"<=",Mf,OK)
 
             else:
                 print(zone[0] + ":", Mr, "<=", Mf, FAIL)
 
 
 generate_report = customtkinter.CTkButton(master=frame, text="Generate Report", command=main)
-generate_report.pack(pady=12, padx=10)
+generate_report.pack(pady=6, padx=5)
 
 exit_button = customtkinter.CTkButton(master=frame, text="Exit", command=exit)
-exit_button.pack(pady=12, padx=10)
+exit_button.pack(pady=6, padx=5)
 
 gui.mainloop()
 main()
